a5800f48c24ae6e231efd623415631b6071d43bf
Waloxis <117771849+Waloxis@users.noreply.github.com>
2024-11-05 12:34:03 -0500

    Update creatinghtml.py

--- scratchproject/creatinghtml.py
+++ scratchproject/creatinghtml.py
@@ -4,21 +4,21 @@ import os
 
 def get_folder_size(folder_path):
     total_size = 0
-    file_names = []  # List to store file names
+    file_names = []  
     for dirpath, dirnames, filenames in os.walk(folder_path):
         for filename in filenames:
             file_path = os.path.join(dirpath, filename)
             
-            if not os.path.islink(file_path):  # Ignore symbolic links
+            if not os.path.islink(file_path):  
                 total_size += os.path.getsize(file_path)
-                file_names.append(filename)  # Add file name to the list
+                file_names.append(filename)  
                 new_path = os.path.join('/Users/walidsphone./Desktop/scratchproject/Games/GameFiles', filename)
                 os.rename(file_path, new_path)
                 
     return total_size, file_names
 
 
-folder_path = "/Users/walidsphone./Desktop/pss"  # Replace with your folder path
+folder_path = "/Path"  
 folder_size, file_names = get_folder_size(folder_path)
 print(f"Total size of the folder '{folder_path}' is: {folder_size} bytes")
 print("File names within the folder:")
